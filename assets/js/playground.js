e0893e8f671650555d98ba72f9aca69bdfb29756
Carrot <59979897+CoronaCarrot@users.noreply.github.com>
2025-03-01 12:37:18 +0000

    MHGHFCYIHYUG

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -80,3 +80,48 @@ htmlEditor.on("inputRead", function(cm, change) {
         autoComplete(cm);
     }
 });
+
+
+
+
+const editor = document.getElementById("editor");
+for (let i = 0; i < editor.children.length; i++) {
+    const element = editor.children[i];
+    // if has class gutter_v 
+    if (element.classList.contains("gutter-v")) {
+        element.addEventListener("mousedown", function(event) {
+            element.style.cursor = "row-resize";
+            const prevElement = editor.children[i - 1];
+            const nextElement = editor.children[i + 1];
+            const startY = event.clientY;
+            const startPrevHeight = prevElement.offsetHeight;
+            const startNextHeight = nextElement.offsetHeight;
+
+            const mouseMove = (e) => {
+                const deltaY = e.clientY - startY;
+                let newPrevHeight = ((startPrevHeight + deltaY) / editor.offsetHeight) * 100;
+                let newNextHeight = ((startNextHeight - deltaY) / editor.offsetHeight) * 100;
+
+                // Ensure minimum size of 8%
+                if (newPrevHeight < 8) {
+                    newPrevHeight = 8;
+                    newNextHeight = ((startPrevHeight + startNextHeight) / editor.offsetHeight) * 100 - newPrevHeight;
+                }
+                if (newNextHeight < 8) {
+                    newNextHeight = 8;
+                    newPrevHeight = ((startPrevHeight + startNextHeight) / editor.offsetHeight) * 100 - newNextHeight;
+                }
+
+                prevElement.style.height = `${newPrevHeight}%`;
+                nextElement.style.height = `${newNextHeight}%`;
+            }
+
+            document.addEventListener("mousemove", mouseMove);
+
+            document.addEventListener("mouseup", () => {
+                element.style.cursor = "default";
+                document.removeEventListener("mousemove", mouseMove);
+            }, { once: true });
+        });
+    }
+}
