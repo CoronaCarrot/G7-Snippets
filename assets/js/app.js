0425c970200fb34c19771a45700cf87bcba1eee8
Carrot <59979897+CoronaCarrot@users.noreply.github.com>
2025-03-11 00:38:54 +0000

    qwdqdw

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,9 +14,27 @@ function requestFullScreen(element) {
 
 // display btn-fullscreen if in iframe
 const btnFullscreen = document.getElementById("btn-fullscreen");
+let fullscreenEnabled = false;
 if (window !== window.parent) {
     btnFullscreen.style.display = "block";
     btnFullscreen.addEventListener("click", (event) => {
+        if (fullscreenEnabled) {
+            // exit full screen
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+            btnFullscreen.src = btnFullscreen.src.replace("closefs", "fullscreen");
+            fullscreenEnabled = false;
+            return;
+        }
         requestFullScreen(document.documentElement);
+        btnFullscreen.src = btnFullscreen.src.replace("fullscreen", "closefs");
+        fullscreenEnabled = true;
     });
 }
